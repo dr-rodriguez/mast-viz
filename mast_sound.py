21be8e5df416f884b73f1f98d65eab555a3ab2ce
David Rodriguez <drodriguez@stsci.edu>
2021-02-01 20:58:41 -0500

    getting a better handle on astronify

--- mast_sound.py
+++ mast_sound.py
@@ -7,7 +7,7 @@ import numpy as np
 from astropy.time import Time
 from astropy.table import Table
 from astronify.series import SoniSeries
-# plt.interactive(False)
+plt.interactive(False)
 
 # Read the data
 df = pd.read_csv('data/mast_time.csv')
@@ -16,17 +16,41 @@ t['year'] = [x.datetime.strftime('%Y-%m') for x in Time(t['date'], format='mjd')
 
 # Quick plots
 plt.plot(t['week'], t['area'])  # area on sky over time
-plt.plot(t['week'], np.log10(t['area']))  # area on sky over time
 plt.plot(t['week'], t['obs_counts'])  # number of observations over time
 plt.plot(t['week'], t['exp_counts'])  # exposure time over time
-
 
 plt.plot(t['year'], t['area'])  # area on sky over time
 plt.xticks(rotation=90)
 ax = plt.gca()
 ax.set_xticks(ax.get_xticks()[::10])
 plt.tight_layout()
-plt.savefig('movie/mast_area.png', dpi=300)
+plt.savefig('image/mast_area.png', dpi=300)
+plt.close()
+
+plt.plot(t['year'], np.log10(t['area']))  # area on sky over time
+plt.xticks(rotation=90)
+ax = plt.gca()
+ax.set_xticks(ax.get_xticks()[::10])
+plt.tight_layout()
+plt.savefig('image/mast_logarea.png', dpi=300)
+plt.close()
+
+plt.plot(t['year'], t['exp_counts'])  # exposure time over time
+plt.xticks(rotation=90)
+ax = plt.gca()
+ax.set_xticks(ax.get_xticks()[::10])
+plt.tight_layout()
+plt.savefig('image/mast_exptime.png', dpi=300)
+plt.close()
+
+plt.plot(t['year'], t['obs_counts'])  # observation counts over time
+plt.xticks(rotation=90)
+ax = plt.gca()
+ax.set_xticks(ax.get_xticks()[::10])
+plt.tight_layout()
+plt.savefig('image/mast_counts.png', dpi=300)
+plt.close()
+
 
 # Notable dates: 1997 for STIS and NICMOS, 2002 for ACS, 2009 for WFC3
 
@@ -34,8 +58,21 @@ plt.savefig('movie/mast_area.png', dpi=300)
 sonidf = SoniSeries(t, time_col='week', val_col='area')
 print(sonidf.pitch_mapper.pitch_map_args)  # check parameters
 sonidf.note_spacing = 0.05  # median seconds between notes; default is 0.01; my video uses 20 frames/sec
-# sonidf.pitch_mapper.pitch_map_args["stretch"] = "log"  # or linear
+sonidf.pitch_mapper.pitch_map_args["stretch"] = "log"  # or linear
+# sonidf.pitch_mapper.pitch_map_args["center_pitch"] = 800  # overall higher pitch
 sonidf.sonify()
 sonidf.play()
 sonidf.stop()
-sonidf.write("movie/mast_area.wav")
+sonidf.write("movie/mast_logarea.wav")
+
+sonidf = SoniSeries(t, time_col='week', val_col='obs_counts')
+sonidf.note_spacing = 0.05
+sonidf.sonify()
+sonidf.write("movie/mast_counts.wav")
+
+sonidf = SoniSeries(t, time_col='week', val_col='exp_counts')
+# sonidf.pitch_mapper.pitch_map_args["stretch"] = "log"
+sonidf.note_spacing = 0.05
+sonidf.sonify()
+sonidf.write("movie/mast_exptime.wav")
+
