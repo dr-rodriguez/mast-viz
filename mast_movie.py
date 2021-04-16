f05b0225b3678b7e8c46b1e81dca1d95c478c1c0
David Rodriguez <drodriguez@stsci.edu>
2021-04-16 14:38:57 -0400

    copying resume logic for main mast movie script

--- mast_movie.py
+++ mast_movie.py
@@ -1,6 +1,7 @@
 # Read the HDF5 stores and generate frames for a movie
 import matplotlib
 matplotlib.use('Qt5Agg')  # avoids crashing MacOS Mojave
+import os
 import numpy as np
 import pandas as pd
 import healpy as hp
@@ -11,7 +12,9 @@ import matplotlib.pyplot as plt
 plt.interactive(False)
 
 # MAST
+print('Loading data...')
 rootname = 'mast'
+movie_dir = 'movie/mast/'
 df1 = pd.read_hdf('data/galex.h5', 'data')
 df2 = pd.read_hdf('data/hst.h5', 'data')
 df3 = pd.read_hdf('data/tess.h5', 'data')
@@ -55,17 +58,41 @@ time_range = range(0, int(max(week_bin))+1)
 # Store some statistics with time
 time_stats = []
 
+print('Starting image loop')
+print(f'{len(time_range)} steps to process...')
+resume = False
+w_resume = 0
+
+# Check if this is a continuing run and re-start from there
+if os.path.isfile(movie_dir + 'temp_time.csv') and os.path.isfile(movie_dir + 'temp_expmap.npy'):
+    print('Resuming from last run')
+
+    # Read temp_time file, convert to a list of dictionaries, and get the maximum week processed
+    temp_time = pd.read_csv(movie_dir + 'temp_time.csv')
+    time_stats = temp_time.to_dict('records')
+    w_resume = temp_time['week'].max()
+
+    # Read temp_smap file
+    exp_map = np.load(movie_dir + 'temp_expmap.npy')
+
+    resume = True
+
 # TODO: Consider an extra loop just to spin around a bit
 # Main loop
 for i in time_range:
     w = time_range[i]
+
+    # Resume after the last processed week
+    if resume and w <= w_resume:
+        continue
+
     area = 0
     obs_counts = 0
     exp_counts = 0
     try:
         # Get data for the week
         week_data = weeks.get_group(w)
-        print(i, w, len(week_data))
+        print(i, w, len(week_data), Time.now())
         title = ''
         for _, row in week_data.iterrows():
             # Get the time for the plot title
@@ -85,8 +112,10 @@ for i in time_range:
             # Skip missing data (eg, bad footprints)
             if len(pix) == 0:
                 continue
+
             pix = pix.iloc[0]  # in case there are duplicate rows
             exp_map[pix['ind']] = exp_map[pix['ind']] + row['t_exptime']
+
             obs_counts += 1
             area += len(pix['ind'])
             exp_counts += row['t_exptime']
@@ -121,13 +150,18 @@ for i in time_range:
                 norm='linear', xsize=1000, title=title)
     hp.projplot(lon, lat, 'r', lonlat=True, coord='G')
     hp.graticule(dpar=45., dmer=30., coord='C', color='lightgray')
-    pngfile1 = 'movie/mast/' + rootname + f'_frame{i:06d}.png'
+    pngfile1 = movie_dir + rootname + f'_frame{i:06d}.png'
     plt.savefig(pngfile1, dpi=300)
     plt.close()
+
+    # Temporary save of time_stats and exp_map to be able to resume processing
+    time_df = pd.DataFrame(time_stats)
+    time_df.to_csv(movie_dir + 'temp_time.csv', index=False)
+    np.save(movie_dir + 'temp_expmap.npy', exp_map)
 
 print("min(MIN), max(MAX) = ", min(MIN), max(MAX))
 # min(MIN), max(MAX) =  0.11 11054835.181203276
 
 # Write out time stats
 time_df = pd.DataFrame(time_stats)
-time_df.to_csv('data/mast_time.csv', index=False)
+time_df.to_csv('data/' + rootname + '_time.csv', index=False)
