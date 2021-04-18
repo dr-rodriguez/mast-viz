cfb199b2795dabe789574c670cbcc95cc0c580fa
David Rodriguez <drodriguez@stsci.edu>
2021-04-18 09:01:30 -0400

    gathering K2 data

--- script.py
+++ script.py
@@ -91,7 +91,6 @@ hp_map = read_map(mapfile='data/galex_map.fits')
 make_plot(hp_map, outfile='image/mast_galex_map.png', title='GALEX')
 # endregion
 
-
 # region PS1
 df = get_db_data(mission='PS1', limit=None,
                  constraints="AND dataproduct_type='image' AND calib_level=3 ",
@@ -116,6 +115,35 @@ make_plot(hp_map, outfile='image/mast_ps1_map.png', title='PS1')
 
 # endregion
 
+# region Kepler
+# No FFI data for Kepler yet
+# TODO: create fake minimalist entry for Kepler?
+# df1 = get_db_data(mission='Kepler', limit=None,
+#                  constraints="AND dataproduct_type='cube' AND calib_level=2 ",
+#                  server='mastdbtest', db='CAOMv230ProdSync')
+df = get_db_data(mission='K2', limit=None,
+                 constraints="AND dataproduct_type='image' AND calib_level=3 ",
+                 server='mastdbtest', db='CAOMv230ProdSync')
+
+# df = pd.concat([df1, df2])
+
+# Using HDF5 to handle the complex data storage without using CSV/FITS (except for the map)
+store = pd.HDFStore("data/kepler.h5")
+store['data'] = df
+
+# Get from the HDF5 rather than DB
+# df = pd.read_hdf("data/ps1.h5", "data")
+
+hp_map, ptab = make_map(df)
+
+store['ptab'] = ptab
+output_map(hp_map, outfile='data/kepler_map.fits')  # in FITS format
+
+hp_map = read_map(mapfile='data/kepler_map.fits')
+store.close()
+
+make_plot(hp_map, outfile='image/mast_kepler_map.png', title='K2')
+# endregion
 
 # Testing changes
 from mast_plot import *
