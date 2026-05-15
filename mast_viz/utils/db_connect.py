5720dca222fdee80b697f34398a3dfa7cf13f8bc
David Rodriguez <drodriguez@stsci.edu>
2026-05-14 20:18:22 -0400

    minor edits

--- mast_viz/utils/db_connect.py
+++ mast_viz/utils/db_connect.py
@@ -23,10 +23,16 @@ def get_db_data(mission="HST", constraints="", limit=None):
         obs_id, obs_collection, instrument_name, dataproduct_type, s_region, t_exptime, t_min, em_min, em_max
         from ObsPointing with (nolock) 
         WHERE obs_collection='{mission}' AND t_exptime IS NOT NULL AND t_min IS NOT NULL AND s_region IS NOT NULL 
-        {constraints} 
-        ORDER BY t_min"""
+        {constraints}"""
 
+    print("Running query...")
     conn = pyodbc.connect(dsn=MAST_DB_SERVER, database=MAST_DB_NAME, trusted_connection="yes", autocommit=True)
     df = pd.read_sql(sql, conn)
+
+    # Sort by t_min
+    print("Sorting by t_min...")
+    df = df.sort_values(by="t_min", ascending=True).reset_index()
+
+    print(f"Returning {len(df)} rows.")
 
     return df
